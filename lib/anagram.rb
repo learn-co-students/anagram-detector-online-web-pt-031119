64c53337bfa915040ef48947f0d701d99d90c9c7
Ryan <kerneltux0@gmail.com>
2019-04-04 11:26:15 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(options)
+
+    options.select do |entry|
+      @word.split('').sort == entry.split('').sort
+        
+    end    
+
+  end 
+  
+end
